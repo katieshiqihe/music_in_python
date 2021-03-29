3971098eaf5ec775142fd419d9195713bcf30295
Katie He <khe35280@gmail.com>
2021-03-28 23:00:39 -0700

    Add sustain pedal

--- utils.py
+++ utils.py
@@ -151,8 +151,41 @@ def get_adsr_weights(frequency, duration, length, decay, sustain_level, sample_r
         weights = np.concatenate((weights, weights[-1]-weights[-1]/tail*np.arange(tail)))
     return weights
 
-def get_song_data(music_notes, note_values, factor, length, decay, sustain_level,
-                  sample_rate=44100, amplitude=4096):
+def apply_pedal(note_values, bar_value):
+    '''
+    Press and hold the sustain pedal throughout the bar.
+
+    Parameters
+    ----------
+    note_values : list
+        List of note duration.
+    bar_value : float
+        Duration of a measure in seconds.
+
+    Returns
+    -------
+    new_values : list
+        List of note duration with sustain.
+
+    '''
+    assert sum(note_values) % bar_value == 0
+    new_values = []
+    start = 0
+    while True:
+        cum_value = np.cumsum(np.array(note_values[start:]))
+        end = np.where(cum_value == bar_value)[0][0]
+        if end == 0:
+            new_values += [note_values[start]]
+        else:
+            this_bar = np.array(note_values[start:start+end+1])
+            new_values += [bar_value-np.sum(this_bar[:i]) for i in range(len(this_bar))]
+        start += end+1
+        if start == len(note_values):
+            break
+    return new_values
+
+def get_song_data(music_notes, note_values, bar_value, factor, length,
+                  decay, sustain_level, sample_rate=44100, amplitude=4096):
     '''
     Generate song from notes. 
 
@@ -162,6 +195,8 @@ def get_song_data(music_notes, note_values, factor, length, decay, sustain_level
         List of note names. 
     note_values : list
         List of note duration.
+    bar_value: float
+        Duration of a bar. 
     factor : list
         Factor to be used to generate overtones.
     length : list
@@ -182,11 +217,18 @@ def get_song_data(music_notes, note_values, factor, length, decay, sustain_level
     '''
     note_freqs = get_piano_notes()
     frequencies = [note_freqs[note] for note in music_notes]
-    song = np.concatenate([apply_overtones(freq, val, factor) 
-                           for freq, val in zip(frequencies, note_values)])
-    weights = np.concatenate([get_adsr_weights(freq, val, length, decay, sustain_level) 
-                              for freq, val in zip(frequencies, note_values)])
+    new_values = apply_pedal(note_values, bar_value)
+    duration = int(sum(note_values)*sample_rate)
+    end_idx = np.cumsum(np.array(note_values)*sample_rate).astype(int)
+    start_idx = np.concatenate(([0], end_idx[:-1]))
+    end_idx = np.array([start_idx[i]+new_values[i]*sample_rate for i in range(len(new_values))]).astype(int)
+    
+    song = np.zeros((duration,))
+    for i in range(len(music_notes)):
+        this_note = apply_overtones(frequencies[i], new_values[i], factor)
+        weights = get_adsr_weights(frequencies[i], new_values[i], length, 
+                                   decay, sustain_level)
+        song[start_idx[i]:end_idx[i]] += this_note*weights
 
-    song = song*weights
     song = song*(amplitude/np.max(song))
     return song
