d8591693146eea01630c3270a64cfe938ffa5399
Katie He <khe35280@gmail.com>
2021-03-28 23:00:56 -0700

    Add sustain pedal

--- twinkle.py
+++ twinkle.py
@@ -53,12 +53,12 @@ factor = [0.68, 0.26, 0.03, 0.  , 0.03]
 length = [0.01, 0.6, 0.29, 0.1]
 decay = [0.05,0.02,0.005,0.1]
 sustain_level = 0.1
-right_hand = utils.get_song_data(right_hand_notes, right_hand_duration,
+right_hand = utils.get_song_data(right_hand_notes, right_hand_duration, 2,
                                  factor, length, decay, sustain_level)
 factor = [0.73, 0.16, 0.06, 0.01, 0.02, 0.01  , 0.01]
 length = [0.01, 0.29, 0.6, 0.1]
 decay = [0.05,0.02,0.005,0.1]
-left_hand = utils.get_song_data(left_hand_notes, left_hand_duration,
+left_hand = utils.get_song_data(left_hand_notes, left_hand_duration, 2,
                                  factor, length, decay, sustain_level)
 data = left_hand+right_hand
 data = data * (4096/np.max(data))
